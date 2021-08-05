38b4650a9c53e76e887746341e5e60f7b90c4f40
Eugene Orlov <evgeniiorlov204@gmail.com>
2021-08-05 21:25:55 +0300

    Fixed bugs with RecyclerView - implemented notifyDataSetChanged

--- app/src/main/java/com/shpp/eorlov/assignment2/utils/MyDiffUtil.kt
+++ app/src/main/java/com/shpp/eorlov/assignment2/utils/MyDiffUtil.kt
@@ -1,12 +1,12 @@
 package com.shpp.eorlov.assignment2.utils
 
 import androidx.recyclerview.widget.DiffUtil
-import com.shpp.eorlov.assignment2.data.PersonData
+import com.shpp.eorlov.assignment2.model.UserData
 
 // pass two list one oldList and second newList
 class MyDiffUtil(
-    private val oldList: List<PersonData?>,
-    private val newList: List<PersonData?>
+    private val oldList: ArrayList<UserData>,
+    private val newList: ArrayList<UserData>
 ) : DiffUtil.Callback() {
 
     override fun getOldListSize(): Int {
@@ -21,14 +21,14 @@ class MyDiffUtil(
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         // compare items based on their unique id
-        return oldList[oldItemPosition]?.email == newList[newItemPosition]?.email
+        return oldList[oldItemPosition].email == newList[newItemPosition].email
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         // in here compare each item if they are same or different
         // return false if any data is same else return true
         return when {
-            oldList[oldItemPosition]?.email != newList[newItemPosition]?.email -> false
+            oldList[oldItemPosition].email != newList[newItemPosition].email -> false
 
             else -> true
         }
